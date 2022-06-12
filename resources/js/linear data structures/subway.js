8862a6939c52678537776d407ac16ecf8ae7e7e1
Lois Gonzales <loisjersong@gmail.com>
2022-06-12 11:20:39 +0800

    added subway.js

---
+++ resources/js/linear data structures/subway.js
@@ -0,0 +1,21 @@
+const DoublyLinkedList = require('./DoublyLinkedList.js');
+
+// Using the Doubly Linked List
+const subway = new DoublyLinkedList();
+subway.addToHead('TimesSquare');
+subway.addToHead('GrandCentral');
+subway.addToHead('CentralPark');
+subway.printList();
+
+subway.addToTail('PennStation');
+subway.addToTail('WallStreet');
+subway.addToTail('BrooklynBridge');
+subway.printList();
+
+subway.removeHead();
+subway.removeTail();
+subway.printList();
+
+subway.removeByData('TimesSquare');
+subway.printList();
+
